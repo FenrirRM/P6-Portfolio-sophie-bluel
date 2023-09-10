53169c14d4333be42a0d0a58cf16cb010d8d1e83
FenrirRM <rehoudja.mael@gmail.com>
2023-09-10 17:57:14 +0200

    1.2 Affichage avec donnée Backend + Filtes

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,89 +1,86 @@
+const gallery = document.querySelector('.gallery');
+
+displayWorks();
+btnfilters();
+
+
 // Fonction d'appel API
 
-async function getworks() {
+async function getWorks() {
+    try {
     const worksResponse = await fetch("http://localhost:5678/api/works");
     return worksResponse.json();
+    } catch (error) {
+        console.log("Erreur lors de la récupération des projets depuis l'API")
+    }
 }
 
-async function getcategories() {
+async function getCategories() {
+    try {
     const categoriesResponse = await fetch("http://localhost:5678/api/categories");
     return await categoriesResponse.json();
+} catch (error) {
+    console.log("Erreur lors de la récupération des catégories depuis l'API")
 }
+}
+
 // Fonction pour affichage dynamiques des éléments
 
-async function displayworks () {
-    const gallery = document.querySelector('.gallery');
-    const dataworks = await getworks();
+async function displayWorks (categorieId) {
+    try {
+        const dataworks = await getWorks();
+        gallery.innerHTML = "";
 
-    dataworks.forEach((works) => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
+            // Création des projets pour l'affichage dans la gallerie
+            dataworks.forEach((works) => {
+                if (categorieId == works.category.id || categorieId == null) {
+                
+                    const figure = document.createElement("figure");
+                    const img = document.createElement("img");
+                    const figcaption = document.createElement("figcaption");
 
-        img.src = works.imageUrl;
-        figcaption.innerText = works.title;
-        figure.setAttribute("categorieId", works.category.id);
+                    img.src = works.imageUrl;
+                    figcaption.innerText = works.title;
+                    figure.setAttribute("categorieId", works.category.id);
 
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
+                    figure.appendChild(img);
+                    figure.appendChild(figcaption);
+                    gallery.appendChild(figure);
+                }
+             })
+    } catch (error) {
+        console.log ("Erreur lors de l'affichage des projets")
+    }
 
-    });
+
 };
-
-displayworks();
-
-// Boutons Filtres
-
-const filters = document.querySelector(".filters");
-
-// Boutons tous
-
-const buttonALL = document.createElement("button");
-buttonALL.setAttribute("class", "filterButton filterButtonselected")
-buttonALL.innerText = "Tous";
-filters.appendChild(buttonALL);
-buttonALL.addEventListener('click',function() {btnALL()});
 
 // Boutons filtres par catégories
 
 async function btnfilters () {
     
-    const dataCategories = await getcategories();
+    const dataCategories = await getCategories();
+    const filters = document.querySelector(".filters");
+
+    // Créations des boutons
     dataCategories.forEach((category) => {
         const btnCategorie =document.createElement("button");
         btnCategorie.innerText = category.name ;
         btnCategorie.setAttribute("class", "filterButton")
-        btnCategorie.setAttribute("categorieId", category.id);
+        btnCategorie.setAttribute("buttonId", category.id);
         filters.appendChild(btnCategorie);
-        btnCategorie.addEventListener('click', function(){filterbycategory(category.id)});
     })
-}
 
-btnfilters();
+    // Ajout d'un event au clic sur chaque bouton
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            let categorieId = button.getAttribute("buttonId");
+            buttons.forEach((button) => button.classList.remove("filterButtonActive"));
+            this.classList.add("filterButtonActive");
+            displayWorks (categorieId);
+        })
+    })
 
-// Fonction pour filtré les projets par catégories
-
-function filterbycategory (id) {
-    const gallery = document.querySelector(".gallery");
-    const works =  gallery.querySelectorAll("figure");
-    works.forEach(element => {
-    if (element.getAttribute("categorieId") != id) {
-      element.style.display = "none"
-    } else {
-      element.style.display = "unset"
-    }
-  });
-}
-
-// Fonction pour affiché tous les projets 
-
-function btnALL () {
-    const gallery = document.querySelector(".gallery");
-    const works =  gallery.querySelectorAll("figure");
-    works.forEach(element => {
-        element.style.display = "unset"
-      }
-    );
 }
 
