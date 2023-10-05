3da0e774bd6619a1684957a97c094cb092a5f447
FenrirRM <rehoudja.mael@gmail.com>
2023-10-06 00:33:36 +0200

    3.3 Formulaire ajout photo non fonctionnel

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -77,14 +77,15 @@ function createWorksModal(works) {
     figureModal.appendChild(iconTrash);
     galleryModal.appendChild(figureModal);
     
+    // Ajout d'un event listener sur l'icon corbeille pour supprimer un projet
     iconTrash.addEventListener("click", (e) => {
-        
-        deleteWorks(works.id);
         e.preventDefault();
+        deleteWorks(works.id);
+        
     });
 }
-// Boutons filtres par catégories
 
+// Boutons filtres par catégories
 async function btnfilters () {
     
     const dataCategories = await getCategories();
@@ -131,6 +132,9 @@ function Admin() {
         
         adminDisplay();
         creatModal();
+        navigateModal();
+        creatOption();
+        inputFiles()
         
     }
 }
@@ -195,7 +199,15 @@ const closeModal = function (e) {
     modal.removeEventListener("click", closeModal);
     modal.querySelector(".js-modal-close").removeEventListener("click", closeModal);
     modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation);
-    modal = null
+    modal = null;
+
+    const modalContent1 = document.querySelector(".modalContent1");
+    const modalContent2 = document.querySelector(".modalContent2");
+    const arrowLeft = document.querySelector(".arrowLeft");
+
+    modalContent1.style.display = "flex";
+    modalContent2.style.display = "none";
+    arrowLeft.style.display = "none";
 };
 
 const stopPropagation = function (e) {
@@ -236,6 +248,7 @@ window.addEventListener("keydown", function (e) {
 });
 };
 
+// Fonction pour supprimer un projet
 async function deleteWorks(workId) {
     const adminToken = sessionStorage.getItem("token")
     try {
@@ -249,7 +262,7 @@ async function deleteWorks(workId) {
             });
         
 
-            if (response.status === 200) {
+            if (response.ok) {
                 console.log("Projet supprimé avec succès.");
                 displayWorks();
             } else if (response.status === 401) {
@@ -259,4 +272,107 @@ async function deleteWorks(workId) {
     } catch (error) {
         console.error("Erreur lors de la requête:", error);
       };
+};
+
+// Fonction pour changer de fenêtre dans la modale
+function navigateModal () {
+    const buttonModal = document.querySelector(".buttonModal");
+    const modalContent1 = document.querySelector(".modalContent1");
+    const modalContent2 = document.querySelector(".modalContent2");
+    const arrowLeft = document.querySelector(".arrowLeft");
+
+    // Pour aller vers la fenêtre d'ajout de projet
+    buttonModal.addEventListener("click", function () {
+        modalContent1.style.display = "none";
+        modalContent2.style.display = "flex";
+        arrowLeft.style.display = "flex";
+      });
+    
+    // Pour aller vers la fenêtre de la gallerie de la modale
+    arrowLeft.addEventListener("click", function () {
+        modalContent1.style.display = "flex";
+        modalContent2.style.display = "none";
+        arrowLeft.style.display = "none";
+      });  
+};
+
+// Fonction pour créer les options pour la selection de catégorie d'ajout photo
+async function creatOption() {
+    const dataCategories = await getCategories();
+    const categorie = document.getElementById("category");
+
+    dataCategories.forEach((category) => {
+        const option = document.createElement("option");
+        option.innerText = category.name;
+        option.value = category.id;
+        option.classList.add("option");
+        categorie.appendChild(option);
+    });
+    
+};
+
+// Fonction pour afficher la preview de l'image input
+function inputFiles() {
+    const viewImage = document.querySelector(".addPhoto");
+    const iconDelete = document.querySelector(".iconDelete");
+    const inputFile = document.getElementById("photoInput");
+    const logoPhoto = document.querySelector(".logoPhoto");
+    const btnPhotoInput = document.querySelector(".btnPhotoInput");
+    const photoInputTxt = document.querySelector(".photoInputTxt");
+    const errorImg = document.querySelector(".errorImg");
+
+    inputFile.addEventListener("change", () => {
+        const file = inputFile.files[0];
+
+        // Erreur si l'image fait plus de 4mo
+        const fileMaxSize = 4 * 1024 * 1024;
+        if (file.size > fileMaxSize) {
+            errorImg.textContent = "Votre image est trop volumineuse";
+            console.log("Image file > 4mo");
+            return;        
+        }
+
+        errorImg.textContent = "";
+        const reader = new FileReader();        
+
+        reader.addEventListener("load", function () {
+
+            // Création d'un nouvel élément `img`
+            const img = document.createElement("img");
+            img.setAttribute("src", reader.result);
+            img.classList.add("imageFile");
+            viewImage.appendChild(img);
+        });
+    
+        reader.readAsDataURL(file);
+
+        // Cacher les éléments de la boite ajout photo et afficher la croix
+        logoPhoto.style.display = "none";
+        btnPhotoInput.style.display = "none";
+        photoInputTxt.style.display = "none";
+        errorImg.style.display = "none";
+        iconDelete.style.display = "flex";
+
+        // Event sur la croix pour enlever le file input
+        iconDelete.addEventListener("click", function () {
+            const imageFile = document.querySelector(".imageFile");
+            
+            // Afficher les éléments de la boite ajout photo et cacher la croix
+            logoPhoto.style.display = "flex";
+            btnPhotoInput.style.display = "flex";
+            photoInputTxt.style.display = "flex";
+            errorImg.style.display = "flex";
+            iconDelete.style.display = "none";
+
+            // Supprimer l'image
+            if (imageFile) {
+                imageFile.remove();
+            }
+
+            inputFile.value = "";
+        });
+
+    });
+    
+      
 };
